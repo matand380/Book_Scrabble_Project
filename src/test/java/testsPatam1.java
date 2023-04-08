a0d9434a8c8d526b1df52d9309ed0853f56bc3d9
DanDavidovich <davidovich.dan@gmail.com>
2023-04-08 20:12:04 +0000

    BSP-21 added single file to run all the tests from patam 1

---
+++ src/test/java/testsPatam1.java
@@ -0,0 +1,12 @@
+public class testsPatam1 {
+    public static void main(String[] args) {
+        new testLRU();
+        new testDictionary();
+        new testCacheManager();
+        new testBloomFliter();
+        new testIOSearch();
+        new testLFU();
+        System.out.println("testsPatam1-done");
+
+    }
+}
