9a01fcfb2360e072418be170a8de5a699e8a72b9
DanDavidovich <davidovich.dan@gmail.com>
2023-04-08 19:50:03 +0000

    added basic Cache tests based on patam 1 project , need to match to a packege after eviatar will add the patam 1 project

---
+++ src/test/java/testCacheManager.java
@@ -0,0 +1,38 @@
+public class testCacheManager {
+    public static void testCacheManager() {
+        CacheManager exists = new CacheManager(3, new LRU());
+        boolean b = exists.query("a");
+        b |= exists.query("b");
+        b |= exists.query("c");
+
+        if (b)
+            System.out.println("wrong result for CacheManager first queries (-5)");
+
+        exists.add("a");
+        exists.add("b");
+        exists.add("c");
+
+        b = exists.query("a");
+        b &= exists.query("b");
+        b &= exists.query("c");
+
+        if (!b)
+            System.out.println("wrong result for CacheManager second queries (-5)");
+
+        boolean bf = exists.query("d"); // false, LRU is "a"
+        exists.add("d");
+        boolean bt = exists.query("d"); // true
+        bf |= exists.query("a"); // false
+        exists.add("a");
+        bt &= exists.query("a"); // true, LRU is "b"
+
+        if (bf || !bt)
+            System.out.println("wrong result for CacheManager last queries (-10)");
+
+    }
+    public static void main(String[] args) {
+        testCacheManager();
+        System.out.println("testCacheManager-done");
+    }
+
+}
