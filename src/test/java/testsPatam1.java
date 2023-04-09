81ff8b8ef859b006f8d7494d92f8a9ab0065c902
DanDavidovich <davidovich.dan@gmail.com>
2023-04-09 09:36:51 +0000

    BSP-21 updated the patam1 test file to include bag and board

--- src/test/java/testsPatam1.java
+++ src/test/java/testsPatam1.java
@@ -1,4 +1,13 @@
 public class testsPatam1 {
+    /**
+     * The main function is used to test the other classes in this package.
+     * It creates an instance of each class and then calls its main function.
+     * The main function prints out an error message if any of these
+     * conditions are not met.
+     *
+     * @return Void
+     *
+     */
     public static void main(String[] args) {
         new testLRU();
         new testDictionary();
@@ -6,6 +15,8 @@ public class testsPatam1 {
         new testBloomFliter();
         new testIOSearch();
         new testLFU();
+        new testBag();
+        new testBoard();
         System.out.println("testsPatam1-done");
 
     }
