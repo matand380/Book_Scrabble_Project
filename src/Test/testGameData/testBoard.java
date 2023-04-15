c1daeeb8b23e0b83333016999b06f73cee7d5d3a
DanDavidovich <davidovich.dan@gmail.com>
2023-04-15 08:46:13 +0000

    BSP-21 added tests to board

--- src/Test/testGameData/testBoard.java
+++ src/Test/testGameData/testBoard.java
@@ -35,48 +35,56 @@ public class testBoard {
         Word w6=new Word(ts,7,0,false);
 
         if( b.boardLegal(w0))
-            System.out.println("your boardLegal function is wrong");
+            System.out.println("your boardLegal function is wrong(w0): " + w0.toString() + " is out of bounds");
 
         if( b.boardLegal(w1))
-            System.out.println("your boardLegal function is wrong");
+            System.out.println("your boardLegal function is wrong(w1): " + w1.toString() + " is out of bounds");
 
         if( b.boardLegal(w2))
-            System.out.println("your boardLegal function is wrong");
+            System.out.println("your boardLegal function is wrong(w2): " + w2.toString() + " is out of bounds");
 
         if( b.boardLegal(w3))
-            System.out.println("your boardLegal function is wrong");
+            System.out.println("your boardLegal function is wrong(w3): " + w3.toString() + " is out of bounds");
 
         if( b.boardLegal(w4))
-            System.out.println("your boardLegal function is wrong");
+            System.out.println("your boardLegal function is wrong(w4): " + w4.toString() + " is out of bounds");
 
         if( !b.boardLegal(w5))
-            System.out.println("your boardLegal function is wrong");
+            System.out.println("your boardLegal function is wrong(w5): " + w5.toString() + " is not out of bounds");
 
         if( !b.boardLegal(w6))
-            System.out.println("your boardLegal function is wrong");
+            System.out.println("your boardLegal function is wrong(w6): " + w6.toString() + " is not out of bounds");
 
         for(Tile t : ts)
             bag.put(t);
 
         Word horn=new Word(get("HORN"), 7, 5, false);
         if(b.tryPlaceWord(horn)!=14)
-            System.out.println("problem in placeWord for 1st word (should be 14)");
+            System.out.println("problem in placeWord for 1st word");
 
         Word farm=new Word(get("FA_M"), 5, 7, true);
         if(b.tryPlaceWord(farm)!=9)
-            System.out.println("problem in placeWord for 2ed word (should be 9) ");
+            System.out.println("problem in placeWord for 2ed word");
 
         Word paste=new Word(get("PASTE"), 9, 5, false);
         if(b.tryPlaceWord(paste)!=25)
-            System.out.println("problem in placeWord for 3ed word (should be 25) ");
+            System.out.println("problem in placeWord for 3ed word");
 
         Word mob=new Word(get("_OB"), 8, 7, false);
         if(b.tryPlaceWord(mob)!=18)
-            System.out.println("problem in placeWord for 4th word (should be 18) ");
+            System.out.println("problem in placeWord for 4th word");
 
         Word bit=new Word(get("BIT"), 10, 4, false);
         if(b.tryPlaceWord(bit)!=22)
-            System.out.println("problem in placeWord for 5th word (should be 22)");
+            System.out.println("problem in placeWord for 5th word ");
+
+        Word illegal=new Word(get("ILLEGAL"), 10, 4, false);
+        if(b.tryPlaceWord(illegal)==-1)
+            System.out.println("problem in placeWord for illegal word");
+
+        Word illegal2=new Word(get("ILLEGAL"), 7, 5, false);
+        if(b.tryPlaceWord(illegal2)==-1)
+            System.out.println("problem in placeWord for illegal2 word");
 
     }
     public static void main(String[] args) {
