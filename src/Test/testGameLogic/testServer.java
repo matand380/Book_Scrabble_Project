671517952d395bbfd426a16323eedba625b02c6b
DanDavidovich <davidovich.dan@gmail.com>
2023-04-17 22:42:02 +0300

    BSP-19 added tests for server based on the test we did to the server part

--- src/Test/testGameLogic/testServer.java
+++ src/Test/testGameLogic/testServer.java
@@ -1,15 +1,23 @@
-package Model.GameLogic;
+package testGameLogic;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import Model.GameLogic.*;
+
+import java.io.*;
 import java.net.Socket;
 import java.util.Random;
 import java.util.Scanner;
 
-public class MainTrain {
+public class testServer {
+    public static void main(String[] args) {
+        if (testServer()) {
+            testDM();
+            testBookScrabbleHandler();
+            System.out.println("testServer-done");
+        } else {
+            System.out.println("testServer-failed");
+        }
+
+    }
 
     public static void client1(int port) throws Exception {
         Socket server = new Socket("localhost", port);
@@ -22,7 +30,7 @@ public class MainTrain {
         outToServer.flush();
         String response = in.next();
         if (response == null || !response.equals(rev))
-            System.out.println("problem getting the right response from your server, cannot continue the test (-100)");
+            System.out.println("problem getting the right response from your server, cannot continue the test");
         in.close();
         outToServer.println(text);
         outToServer.close();
@@ -33,13 +41,13 @@ public class MainTrain {
         boolean ok = true;
         Random r = new Random();
         int port = 6000 + r.nextInt(1000);
-        MyServer s = new MyServer(port, new ClientHandler1());
+        MyServer s = new MyServer(port, new testServer.ClientHandler1());
         int c = Thread.activeCount();
         s.start(); // runs in the background
         try {
             client1(port);
         } catch (Exception e) {
-            System.out.println("some exception was thrown while testing your server, cannot continue the test (-100)");
+            System.out.println("some exception was thrown while testing your server, cannot continue the test");
             ok = false;
         }
         s.close();
@@ -47,10 +55,11 @@ public class MainTrain {
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
         if (Thread.activeCount() != c) {
-            System.out.println("you have a thread open after calling close method (-100)");
+            System.out.println("you have a thread open after calling close method");
             ok = false;
         }
         return ok;
@@ -58,7 +67,7 @@ public class MainTrain {
 
     public static String[] writeFile(String name) {
         Random r = new Random();
-        String txt[] = new String[10];
+        String[] txt = new String[10];
         for (int i = 0; i < txt.length; i++)
             txt[i] = "" + (10000 + r.nextInt(10000));
 
@@ -76,27 +85,22 @@ public class MainTrain {
     }
 
     public static void testDM() {
-        String t1[] = writeFile("t1.txt");
-        String t2[] = writeFile("t2.txt");
-        String t3[] = writeFile("t3.txt");
+        String[] t1 = writeFile("t1.txt");
+        String[] t2 = writeFile("t2.txt");
+        String[] t3 = writeFile("t3.txt");
 
         DictionaryManager dm = DictionaryManager.get();
 
-        if (!dm.query("t1.txt", "t2.txt", t2[4]))
-            System.out.println("problem for Dictionary Manager query (-5)");
-        if (!dm.query("t1.txt", "t2.txt", t1[9]))
-            System.out.println("problem for Dictionary Manager query (-5)");
-        if (dm.query("t1.txt", "t3.txt", "2" + t3[2]))
-            System.out.println("problem for Dictionary Manager query (-5)");
-        if (dm.query("t2.txt", "t3.txt", "3" + t2[5]))
-            System.out.println("problem for Dictionary Manager query (-5)");
+        if (!dm.query("t1.txt", "t2.txt", t2[4])) System.out.println("1 problem for Dictionary Manager query ");
+        if (!dm.query("t1.txt", "t2.txt", t1[9])) System.out.println("2 problem for Dictionary Manager query");
+        if (dm.query("t1.txt", "t3.txt", "2" + t3[2])) System.out.println("3 problem for Dictionary Manager query");
+        if (dm.query("t2.txt", "t3.txt", "3" + t2[5])) System.out.println("4 problem for Dictionary Manager query");
         if (!dm.challenge("t1.txt", "t2.txt", "t3.txt", t3[2]))
-            System.out.println("problem for Dictionary Manager challenge (-5)");
+            System.out.println("5 problem for Dictionary Manager challenge");
         if (dm.challenge("t2.txt", "t3.txt", "t1.txt", "3" + t2[5]))
-            System.out.println("problem for Dictionary Manager challenge (-5)");
+            System.out.println("6 problem for Dictionary Manager challenge ");
 
-        if (dm.getSize() != 3)
-            System.out.println("wrong size for the Dictionary Manager (-10)");
+        if (dm.getSize() != 3) System.out.println("wrong size for the Dictionary Manager");
 
     }
 
@@ -118,9 +122,9 @@ public class MainTrain {
         }
     }
 
-    public static void testBSCH() {
-        String s1[] = writeFile("s1.txt");
-        String s2[] = writeFile("s2.txt");
+    public static void testBookScrabbleHandler() {
+        String[] s1 = writeFile("s1.txt");
+        String[] s2 = writeFile("s2.txt");
 
         Random r = new Random();
         int port = 6000 + r.nextInt(1000);
@@ -134,13 +138,6 @@ public class MainTrain {
         s.close();
     }
 
-    public static void main(String[] args) {
-        if (testServer()) {
-            testDM();
-            testBSCH();
-        }
-        System.out.println("done");
-    }
 
     public static class ClientHandler1 implements ClientHandler {
         PrintWriter out;
@@ -151,7 +148,7 @@ public class MainTrain {
             out = new PrintWriter(outToClient);
             in = new Scanner(inFromclient);
             String text = in.next();
-            out.println(new StringBuilder(text).reverse().toString());
+            out.println(new StringBuilder(text).reverse());
             out.flush();
         }
 
