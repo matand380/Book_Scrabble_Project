76b0bbc392df44791e0da222b94d32aa69d6ede1
DanDavidovich <davidovich.dan@gmail.com>
2023-05-16 23:05:04 +0300

    adding test files

---
+++ src/Test/test_Comunication/Test_Guest_Model.java
@@ -0,0 +1,14 @@
+package test_Comunication;
+
+import Model.BS_Guest_Model;
+
+public class Test_Guest_Model {
+    public static void main(String[] args) {
+        BS_Guest_Model client1 = BS_Guest_Model.getModel();
+        client1.openSocket("127.0.0.1", 65533);  //copy local server ip + server port
+        client1.getCommunicationHandler().setCom();
+
+//      BS_Guest_Model client2 = BS_Guest_Model.getModel();
+//      client2.openSocket("127.0.0.1", 65533);  //copy local server ip + server port
+//      client2.getCommunicationHandler().setCom();
+}
