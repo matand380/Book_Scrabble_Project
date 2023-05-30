b9c014644d789fd1d4fec58b72bf79a6690a0842
Eviatar109 <Eviatar109@gmail.com>
2023-05-30 21:30:40 +0300

    Start to build the map in the view model (client)

--- src/main/java/BookScrabbleApp/ViewModel/BS_ViewModel.java
+++ src/main/java/BookScrabbleApp/ViewModel/BS_ViewModel.java
@@ -2,10 +2,74 @@ package BookScrabbleApp.ViewModel;
 
 import BookScrabbleApp.Model.BS_Host_Model;
 
-import java.util.Observable;
-import java.util.Observer;
+import java.util.*;
+import java.util.function.*;
 
 public class BS_ViewModel extends Observable implements Observer {
+
+    private Map<String, Consumer<String>> viewCommandsFromClient = new HashMap<>();
+
+    public BS_ViewModel() {
+        viewCommandsFromClient.put("hand", message -> {
+            //The hand of the player is updated
+
+            // TODO: 30/05/2023 update the hand in the view
+        });
+
+        // FIXME: 30/05/2023: tileBoard or board?
+        viewCommandsFromClient.put("tileBoard", message -> {
+            //The board is updated
+            // TODO: 30/05/2023 update the board in the view
+        });
+
+        viewCommandsFromClient.put("passTurn", message -> {
+            //The turn is updated
+            // TODO: 30/05/2023 update the turn in the view
+            // TODO: 30/05/2023 test if the player is the current player and if so, enable the buttons
+        });
+
+        viewCommandsFromClient.put("wordsForChallenge", message -> {
+            //Words for challenge is coming
+            // TODO: 30/05/2023 update the player with the words challenge in the view
+        });
+
+        viewCommandsFromClient.put("playersScores", message -> {
+            //The scores of the players are updated
+            // TODO: 30/05/2023 update the scores in the view
+        });
+
+        viewCommandsFromClient.put("invalidWord", message -> {
+            //The word is invalid
+            // TODO: 30/05/2023 pop up the word is invalid in the view
+        });
+
+        viewCommandsFromClient.put("challengeAlreadyActivated", message -> {
+            //The challenge is already activated
+            // TODO: 30/05/2023 pop up the 'challenge is already activated' in the view
+        });
+
+        viewCommandsFromClient.put("winner", message -> {
+            // TODO: 30/05/2023 pop up the winner in the view
+        });
+
+        viewCommandsFromClient.put("endGame", message -> {
+            // TODO: 30/05/2023  show the winner in the view
+            // TODO: 30/05/2023  show end game window in the view
+        });
+
+        // FIXME: 30/05/2023 check if we need this function
+        viewCommandsFromClient.put("sortAndSetIndex", message -> {
+            //The index of the player is updated
+            //??????
+        });
+
+        // FIXME: 30/05/2023 check if we need this function
+        viewCommandsFromClient.put("challengeSuccess", message -> {
+            //The challenge is successful
+            //pop up the challenge is successful in the view
+            //?????
+        });
+    }
 
     String winner;
 
@@ -14,13 +78,12 @@ public class BS_ViewModel extends Observable implements Observer {
         if (o == BS_Host_Model.getModel())
             if (arg instanceof String) {
                 String key = (String) arg;
-
-                switch (key) {
-                    case "try successful":
-                        //update the host view
-
-                        break;
-                }
+                String[] message = key.split(":");
+                String command = message[0];
+                if (viewCommandsFromClient.containsKey(command))
+                    viewCommandsFromClient.get(command).accept(key);
+                else
+                    System.out.println("Command not found");
             }
 
 
