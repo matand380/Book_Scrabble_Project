7ce5a5a545824ff10da7d1b9717e553c611be042
matand380 <matand94@cs.colman.ac.il>
2023-05-30 10:32:45 +0300

    arrangements Facades

--- src/main/java/BookScrabbleApp/Model/BookScrabbleGuestFacade.java
+++ src/main/java/BookScrabbleApp/Model/BookScrabbleGuestFacade.java
@@ -17,69 +17,65 @@ public class BookScrabbleGuestFacade extends Observable implements Observer {
         guestModel.addObserver(this);
     }
 
+
+    //do something methods
     public void openSocket(String ip, int port) {
         guestModel.openSocket(ip, port);
     }
-
     public void setPlayersScores(String[] playersScores) {
         guestModel.setPlayersScores(playersScores);
     }
-
     public void passTurn(int playerIndex) {
         guestModel.passTurn(playerIndex);
     }
-
     public void tryPlaceWord(String word, int x, int y, boolean isVertical) {
         guestModel.tryPlaceWord(word, x, y, isVertical);
     }
-
     public void challengeWord(String word) {
         guestModel.challengeWord(word);
     }
-
-    public boolean isHost() {
-        return guestModel.isHost();
-    }
-
     public void setBoard(Tile[][] boardTiles) {
         guestModel.setBoard(boardTiles);
     }
-
-    public Socket getSocket() {
-        return guestModel.getSocket();
-    }
-
-    public Player getPlayer() {
-        return guestModel.getPlayer();
-    }
-
     public void setPlayerProperties(String name) {
         guestModel.setPlayerProperties(name);
     }
-
-    public String[] getPlayersScores() {
-        return guestModel.getPlayersScores();
+    public void endGame() {
+        guestModel.endGame();
     }
-    
+
+
+    //get something
+    public boolean isHost() {
+        return guestModel.isHost();
+    }
+    public Socket getSocket() {
+        return guestModel.getSocket();
+    }
+    public Player getPlayer() {
+        return guestModel.getPlayer();
+    }
     public ClientCommunicationHandler getCommunicationHandler() {
         return guestModel.getCommunicationHandler();
     }
-
     public int getCurrentPlayerScore() {
         return guestModel.getCurrentPlayerScore();
     }
 
+    //VIEW
+    //scores array
+    public String[] getPlayersScores() {
+        return guestModel.getPlayersScores();
+    }
+    //hand
     public List<Tile> getCurrentPlayerHand() {
         return guestModel.getCurrentPlayerHand();
     }
-
+    //board
     public Tile[][] getBoardState() {
         return guestModel.getBoardState();
     }
 
-    public void endGame() {
-        guestModel.endGame();
-    }
 
     @Override
     public void update(Observable o, Object arg) {
--- src/main/java/BookScrabbleApp/Model/BookScrabbleHostFacade.java
+++ src/main/java/BookScrabbleApp/Model/BookScrabbleHostFacade.java
@@ -14,103 +14,89 @@ public class BookScrabbleHostFacade extends Observable implements Observer {
         hostModel.addObserver(this);
     }
 
+
+    //do something methods
     public void passTurn(int playerIndex) {
         hostModel.passTurn(playerIndex);
     }
-
-
-    public Player getPlayer() {
-        return hostModel.getPlayer();
+    public void setChallengeInfo(String challengeInfo) {
+        hostModel.setChallengeInfo(challengeInfo);
     }
-
     public void tryPlaceWord(Word word) {
         hostModel.tryPlaceWord(word);
     }
-
-
     public void openSocket(String ip, int port) {
         hostModel.openSocket(ip, port);
     }
-
     public void addPlayer(Player p) {
         hostModel.addPlayer(p);
     }
-
     public void startNewGame() {
         hostModel.startNewGame();
     }
-
-    public boolean isFull() {
-        return hostModel.isFull();
-    }
-
-    public List<Player> getPlayers() {
-        return hostModel.getPlayers();
-    }
-
-    public Queue<Player> getScores() {
-        return hostModel.getScoresManager();
-    }
-
-    public Map<String, String> getPlayerToSocketID() {
-        return hostModel.getPlayerToSocketID();
-    }
-
     public void setPlayerProperties(String name) {
         hostModel.setPlayerProperties(name);
     }
-
     public void requestChallengeActivation(String challengeInfo) {
         hostModel.requestChallengeActivation(challengeInfo);
     }
-
-    public Socket getGameSocket() {
-        return hostModel.getGameSocket();
-    }
-
-    public MyServer getCommunicationServer() {
-        return hostModel.getCommunicationServer();
-    }
-
-    public int getCurrentPlayerIndex() {
-        return hostModel.getCurrentPlayerIndex();
-    }
-
     public void setNextPlayerIndex(int index) {
         hostModel.setNextPlayerIndex(index);
     }
 
+
+    //get something
+    public Socket getGameSocket() {
+        return hostModel.getGameSocket();
+    }
+    public MyServer getCommunicationServer() {
+        return hostModel.getCommunicationServer();
+    }
+    public int getCurrentPlayerIndex() {
+        return hostModel.getCurrentPlayerIndex();
+    }
     public String getChallengeInfo() {
         return hostModel.getChallengeInfo();
     }
-
-    public void setChallengeInfo(String challengeInfo) {
-        hostModel.setChallengeInfo(challengeInfo);
-    }
-
     public int getCurrentPlayerScore() {
         return hostModel.getCurrentPlayerScore();
     }
+    public boolean isGameIsOver() {
+        return hostModel.gameIsOver;
+    }
+    public boolean isFull() {
+        return hostModel.isFull();
+    }
+    public void endGame() {
+        hostModel.endGame();
+    }
+    public Player getPlayer() {
+        return hostModel.getPlayer();
+    }
+    public List<Player> getPlayers() {
+        return hostModel.getPlayers();
+    }
+    public Queue<Player> getScores() {
+        return hostModel.getScoresManager();
+    }
+    public Map<String, String> getPlayerToSocketID() {
+        return hostModel.getPlayerToSocketID();
+    }
 
+    //VIEW
+    //scores array
+    public String[] getPlayersScores() {
+        return hostModel.getPlayersScores();
+    }
+    //hand
     public List<Tile> getCurrentPlayerHand() {
         return hostModel.getCurrentPlayerHand();
     }
-
+    //board
     public Tile[][] getBoardState() {
         return hostModel.getBoardState();
     }
 
-    public boolean isGameIsOver() {
-        return hostModel.gameIsOver;
-    }
-
-    public String[] getPlayersScores() {
-        return hostModel.getPlayersScores();
-    }
-
-    public void endGame() {
-        hostModel.endGame();
-    }
 
     @Override
     public void update(Observable o, Object arg) {
