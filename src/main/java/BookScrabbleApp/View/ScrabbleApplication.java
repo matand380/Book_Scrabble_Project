a53f02106773cbaab9b27c5b75085bf1d94133fe
DanDavidovich <davidovich.dan@gmail.com>
2023-06-04 14:26:23 +0300

    another version of FXMLLoader

--- src/main/java/BookScrabbleApp/View/ScrabbleApplication.java
+++ src/main/java/BookScrabbleApp/View/ScrabbleApplication.java
@@ -10,20 +10,32 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class ScrabbleApplication extends Application  {
-    @Override
-    public void start(Stage primaryStage) throws IOException {
-        primaryStage.setTitle("Hello World");
-        Group root = new Group();
-        Scene scene = new Scene(root, 300, 250);
-        Button btn = new Button();
-        btn.setLayoutX(100);
-        btn.setLayoutY(80);
-        btn.setText("Hello World");
-        btn.setOnAction( actionEvent ->
-                System.out.println("Hello World"));
-        root.getChildren().add(btn);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+//    @Override
+//    public void start(Stage primaryStage) throws IOException {
+//        primaryStage.setTitle("Hello World");
+//        Group root = new Group();
+//        Scene scene = new Scene(root, 300, 250);
+//        Button btn = new Button();
+//        btn.setLayoutX(100);
+//        btn.setLayoutY(80);
+//        btn.setText("Hello World");
+//        btn.setOnAction( actionEvent ->
+//                System.out.println("Hello World"));
+//        root.getChildren().add(btn);
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
+//    }
+@Override
+public void start(Stage stage) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(ScrabbleApplication.class.getResource("View/hello-view.fxml"));
+    Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//    stage.setTitle("Hello!");
+    stage.setScene(scene);
+    stage.show();
+}
+
+    public static void main(String[] args) {
+    launch(args);
     }
 
 }
