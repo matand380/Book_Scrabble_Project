ff3977358a5ef82eccbe7e23446faeb69fb92fad
Eviatar109 <Eviatar109@gmail.com>
2023-06-08 14:23:58 +0300

    BSP-101 - add a controller for BookScrabbleApp

---
+++ src/main/java/BookScrabbleApp/View/welcomeController.java
@@ -0,0 +1,33 @@
+package BookScrabbleApp.View;
+
+import BookScrabbleApp.ViewModel.*;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.stage.*;
+
+public class welcomeController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    private Label welcomeText;
+
+
+    @FXML
+    public void switchToHostWindow() throws Exception {
+        root = FXMLLoader.load(getClass().getResource("/com/example/tsetViewModel/hostWindow.fxml"));
+        stage = (Stage) welcomeText.getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    public void switchToGuestWindow() throws Exception {
+        root = FXMLLoader.load(getClass().getResource("/com/example/tsetViewModel/guestWindow.fxml"));
+        stage = (Stage) welcomeText.getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
